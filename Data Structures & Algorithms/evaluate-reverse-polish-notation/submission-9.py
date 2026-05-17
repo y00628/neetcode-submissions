c543132b9c5b2f6e9731b1043a22e7d0e4ad2f37
Yosen Lin <104092281+y00628@users.noreply.github.com>
2026-05-17 05:36:06 -0400

    Add: evaluate-reverse-polish-notation - submission-9

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-9.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        
+        st = deque([])
+        exp = {"+", "-", "*", "/"}
+
+        for tk in tokens:
+            # print(st)
+            if tk not in exp:
+                st.append(tk)
+                continue
+
+            right, left = int(st.pop()), int(st.pop())
+
+            # print(f'{left} {tk} {right}')
+
+            if tk == "+":
+                st.append(str(left+right))
+            elif tk == "-":
+                st.append(str(left-right))
+            elif tk == "*":
+                st.append(str(left*right))
+            elif left%right != 0 and left/right < 0:
+                st.append(str(left//right+1))
+            else:
+                st.append(str(left//right))
+
+        return int(st.pop())
+            
+                
