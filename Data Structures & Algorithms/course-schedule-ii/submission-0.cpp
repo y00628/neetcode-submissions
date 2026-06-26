bbf58f179cb7a2b3c1a936c1f9a6a1d5151a4ce2
Yosen Lin <104092281+y00628@users.noreply.github.com>
2026-06-26 05:55:59 -0400

    Add: course-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+
+        unordered_map<int, vector<int>> adj; // b key
+        unordered_map<int, int> indegree;
+        queue<int> remaining;
+        vector<int> result;
+
+        // build adj list + indegree
+        for (auto& prereq: prerequisites) {
+            adj[prereq[1]].push_back(prereq[0]);
+            indegree[prereq[0]]++;
+        }
+
+        // add those without prereq to queue
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                remaining.push(i);
+            }
+        }
+
+        // going through the topological sort
+        while (!remaining.empty()) {
+            int course = remaining.front();
+            remaining.pop();
+            result.push_back(course);
+
+            for (auto& neighbor: adj[course]) {
+                indegree[neighbor]--;
+
+                if (indegree[neighbor] == 0) {
+                    remaining.push(neighbor);
+                }
+            }
+        }
+
+        if (result.size() == numCourses) {
+            return result;
+        }
+
+        return {};
+
+    }
+};
